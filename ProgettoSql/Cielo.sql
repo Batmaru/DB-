de78fa37f030e645ab498881aa3d024994dd9ca0
Batmaru <marwanrafik02@gmail.com>
2024-09-22 23:34:05 +0200

    cielo.sql

--- ProgettoSql/Cielo.sql
+++ ProgettoSql/Cielo.sql
@@ -1,193 +1,110 @@
-CREATE TYPE Strutturato AS ENUM ('Ricercatore', 'Professore Associato', 'Professore Ordinario');
-CREATE TYPE LavoroProgetto AS ENUM ('Ricerca e Sviluppo', 'Dimostrazione', 'Management', 'Altro');
-CREATE TYPE LavoroNonProgettuale AS ENUM ('Didattica', 'Ricerca', 'Missione', 'Incontro Dipartimentale', 'Incontro Accademico', 'Altro');
-CREATE TYPE CausaAssenza AS ENUM ('Chiusura Universitaria', 'Maternità', 'Malattia');
+CREATE DOMAIN PosInteger AS INTEGER CHECK (VALUE >= 0);
+CREATE DOMAIN StringaM AS VARCHAR(100);
+CREATE DOMAIN CodIATA AS CHAR(3);
 
-CREATE DOMAIN PosInteger AS INTEGER DEFAULT 0 CHECK (VALUE >= 0);
-CREATE DOMAIN StringaM AS VARCHAR(100) DEFAULT '';
-CREATE DOMAIN NumeroOre AS INTEGER DEFAULT 0 CHECK (VALUE >= 0 AND VALUE <= 8);
-CREATE DOMAIN Denaro AS REAL DEFAULT 0 CHECK (VALUE >= 0);
-
-CREATE TABLE Persona (
-    id PosInteger NOT NULL,
-    nome StringaM NOT NULL,
-    cognome StringaM NOT NULL,
-    posizione Strutturato NOT NULL,
-    stipendio Denaro NOT NULL,
-    PRIMARY KEY (id)
+CREATE TABLE Compagnia (
+    nome StringaM PRIMARY KEY,
+    annoFondaz PosInteger,
+    tipo StringaM CHECK (tipo IN ('LowCost', 'Tradizionale', 'Charter'))
 );
 
-CREATE TABLE Progetto (
-    id PosInteger NOT NULL,
-    nome StringaM NOT NULL,
-    inizio DATE NOT NULL,
-    fine DATE NOT NULL,
-    budget Denaro NOT NULL,
-    PRIMARY KEY (id),
-    UNIQUE (nome),
-    CHECK (inizio < fine)
+CREATE TABLE Aeroporto (
+    codice CodIATA PRIMARY KEY,
+    nome StringaM
 );
 
-CREATE TABLE WP (
-    progetto PosInteger NOT NULL,
-    id PosInteger NOT NULL,
-    nome StringaM NOT NULL,
-    inizio DATE NOT NULL,
-    fine DATE NOT NULL,
-    PRIMARY KEY (progetto, id),
-    UNIQUE (progetto, nome),
-    CHECK (inizio < fine),
-    FOREIGN KEY (progetto) REFERENCES Progetto(id)
+CREATE TABLE LuogoAeroporto (
+    aeroporto CodIATA,
+    citta StringaM,
+    nazione StringaM,
+    PRIMARY KEY (aeroporto),
+    FOREIGN KEY (aeroporto) REFERENCES Aeroporto(codice)
 );
 
-CREATE TABLE AttivitaProgetto (
-    id PosInteger NOT NULL,
-    persona PosInteger NOT NULL,
-    progetto PosInteger NOT NULL,
-    wp PosInteger NOT NULL,
-    giorno DATE NOT NULL,
-    tipo LavoroProgetto NOT NULL,
-    oreDurata NumeroOre NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (persona) REFERENCES Persona(id),
-    FOREIGN KEY (progetto, wp) REFERENCES WP(progetto, id)
+CREATE TABLE Volo (
+    codice PosInteger,
+    comp StringaM,
+    durataMinuti PosInteger,
+    PRIMARY KEY (codice, comp),
+    FOREIGN KEY (comp) REFERENCES Compagnia(nome)
 );
 
-CREATE TABLE AttivitaNonProgettuale (
-    id PosInteger NOT NULL,
-    persona PosInteger NOT NULL,
-    tipo LavoroNonProgettuale NOT NULL,
-    giorno DATE NOT NULL,
-    oreDurata NumeroOre NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (persona) REFERENCES Persona(id)
+CREATE TABLE ArrPart (
+    codice PosInteger,
+    comp StringaM,
+    arrivo CodIATA,
+    partenza CodIATA,
+    PRIMARY KEY (codice, comp),
+    FOREIGN KEY (codice, comp) REFERENCES Volo(codice, comp),
+    FOREIGN KEY (arrivo) REFERENCES Aeroporto(codice),
+    FOREIGN KEY (partenza) REFERENCES Aeroporto(codice)
 );
 
-CREATE TABLE Assenza (
-    id PosInteger NOT NULL,
-    persona PosInteger NOT NULL,
-    tipo CausaAssenza NOT NULL,
-    giorno DATE NOT NULL,
-    PRIMARY KEY (id),
-    UNIQUE (persona, giorno),
-    FOREIGN KEY (persona) REFERENCES Persona(id)
-);
+SELECT DISTINCT Volo.codice, Volo.comp
+FROM Volo, ArrPart
+WHERE Volo.codice = ArrPart.codice
+  AND Volo.durataMinuti > 180;
 
--- Query di esempio
+SELECT DISTINCT Volo.comp
+FROM Volo
+WHERE Volo.durataMinuti > 180;
 
-SELECT DISTINCT cognome
-FROM Persona;
+SELECT DISTINCT Volo.codice, Volo.comp
+FROM Volo, ArrPart
+WHERE Volo.codice = ArrPart.codice
+  AND ArrPart.partenza = 'CIA';
 
-SELECT nome, cognome
-FROM Persona
-WHERE posizione = 'Ricercatore';
+SELECT DISTINCT Volo.comp
+FROM Volo, ArrPart
+WHERE Volo.codice = ArrPart.codice
+  AND ArrPart.arrivo = 'FCO';
 
-SELECT cognome
-FROM Persona
-WHERE posizione = 'Professore Associato' AND UPPER(cognome) LIKE 'V%';
+SELECT DISTINCT Volo.codice, Volo.comp
+FROM Volo, ArrPart
+WHERE Volo.codice = ArrPart.codice
+  AND ArrPart.partenza = 'FCO' 
+  AND ArrPart.arrivo = 'JFK';
 
-SELECT cognome
-FROM Persona
-WHERE (posizione = 'Professore Associato' OR posizione = 'Professore Ordinario') AND UPPER(cognome) LIKE 'V%';
+SELECT DISTINCT Volo.comp
+FROM Volo, ArrPart
+WHERE Volo.codice = ArrPart.codice
+  AND ArrPart.partenza = 'FCO' 
+  AND ArrPart.arrivo = 'JFK';
 
-SELECT nome
-FROM Progetto
-WHERE fine <= NOW();
+SELECT DISTINCT Volo.comp
+FROM Volo, ArrPart, LuogoAeroporto AS LA_Roma, LuogoAeroporto AS LA_NewYork
+WHERE Volo.codice = ArrPart.codice
+  AND ArrPart.partenza = LA_Roma.aeroporto
+  AND ArrPart.arrivo = LA_NewYork.aeroporto
+  AND LA_Roma.citta = 'Roma'
+  AND LA_NewYork.citta = 'New York';
 
-SELECT nome
-FROM Progetto
-ORDER BY inizio ASC;
+SELECT DISTINCT Aeroporto.codice, Aeroporto.nome, LuogoAeroporto.citta
+FROM Aeroporto, ArrPart, Compagnia
+WHERE Aeroporto.codice = ArrPart.partenza
+  AND ArrPart.comp = 'MagicFly';
 
-SELECT nome
-FROM WP
-ORDER BY nome ASC;
+SELECT DISTINCT Volo.codice, Volo.comp, ArrPart.partenza, ArrPart.arrivo
+FROM Volo, ArrPart, LuogoAeroporto AS LA_Roma, LuogoAeroporto AS LA_NewYork
+WHERE Volo.codice = ArrPart.codice
+  AND ArrPart.partenza = LA_Roma.aeroporto
+  AND ArrPart.arrivo = LA_NewYork.aeroporto
+  AND LA_Roma.citta = 'Roma'
+  AND LA_NewYork.citta = 'New York';
 
-SELECT DISTINCT tipo
-FROM Assenza;
+SELECT Volo1.comp, Volo1.codice AS codice_volo_1, ArrPart1.partenza, ArrPart2.partenza AS scalo, Volo2.codice AS codice_volo_2, ArrPart2.arrivo
+FROM Volo AS Volo1, ArrPart AS ArrPart1, Volo AS Volo2, ArrPart AS ArrPart2
+WHERE Volo1.codice = ArrPart1.codice
+  AND Volo2.codice = ArrPart2.codice
+  AND ArrPart1.arrivo = ArrPart2.partenza
+  AND ArrPart1.partenza IN (SELECT aeroporto FROM LuogoAeroporto WHERE citta = 'Roma')
+  AND ArrPart2.arrivo IN (SELECT aeroporto FROM LuogoAeroporto WHERE citta = 'New York')
+  AND Volo1.comp = Volo2.comp;
 
-SELECT DISTINCT tipo
-FROM AttivitaProgetto;
-
-SELECT DISTINCT giorno
-FROM AttivitaNonProgettuale
-WHERE tipo = 'Didattica'
-ORDER BY giorno ASC;
-
-SELECT WP.nome, WP.inizio, WP.fine
-FROM WP as wp, Progetto as p
-WHERE p.nome = 'Pegasus'
-    AND wp.progetto = p.id;
-
-SELECT DISTINCT Persona.nome, Persona.cognome, Persona.posizione
-FROM Progetto as pr, AttivitaProgetto as a, Persona as p
-WHERE p.id = a.persona AND pr.nome = 'Pegasus'
-ORDER BY Persona.cognome DESC;
-
-SELECT DISTINCT Persona.nome, Persona.cognome, Persona.posizione
-FROM Persona as p, AttivitaProgetto as a1, AttivitaProgetto as a2, Progetto as pr
-WHERE pr.nome = 'Pegasus'
-AND a1.progetto = pr.id
-AND a2.progetto = pr.id
-AND a1.persona = p.id
-AND a2.persona = p.id
-AND a1.id <> a2.id;
-
-SELECT DISTINCT Persona.nome, Persona.cognome, Persona.posizione
-FROM Persona as p, Assenza as a
-WHERE p.posizione = 'Professore Ordinario'
-AND p.id = a.persona
-AND a.tipo = 'Malattia';
-
-SELECT DISTINCT Persona.nome, Persona.cognome, Persona.posizione
-FROM Persona as p, Assenza as A1, Assenza as A2
-WHERE p.posizione = 'Professore Ordinario'
-AND A1.Persona = p.id
-AND A2.Persona = p.id
-AND A1.tipo = 'Malattia'
-AND A2.tipo = 'Malattia'
-AND A1.id <> A2.id;
-
-SELECT DISTINCT Persona.nome, Persona.cognome, Persona.posizione
-FROM Persona, AttivitaNonProgettuale as A
-WHERE A.persona = Persona.id
-AND A.tipo = 'Didattica'
-AND Persona.posizione = 'Ricercatore';
-
-SELECT DISTINCT p.nome, p.cognome, p.posizione
-FROM Persona as p, AttivitaNonProgettuale as A1, AttivitaNonProgettuale as A2
-WHERE A1.persona = p.id
-AND A2.persona = p.id
-AND A1.tipo = 'Didattica'
-AND A2.tipo = 'Didattica'
-AND A1.id <> A2.id;
-
-SELECT DISTINCT p.nome, p.cognome
-FROM Persona as p, AttivitaProgetto as ap, AttivitaNonProgettuale as anp
-WHERE ap.persona = p.id
-AND anp.persona = p.id
-AND ap.giorno = anp.giorno;
-
-SELECT DISTINCT p.nome, p.cognome, ap.giorno, pr.nome AS nome_progetto, anp.tipo AS tipo_attivita_non_progettuale, ap.oreDurata AS durata_attivita_progettuale, anp.oreDurata AS durata_attivita_non_progettuale
-FROM Persona as p, AttivitaProgetto as ap, AttivitaNonProgettuale as anp, Progetto as pr
-WHERE ap.persona = p.id
-AND anp.persona = p.id
-AND ap.giorno = anp.giorno
-AND ap.progetto = pr.id;
-
-SELECT DISTINCT p.nome, p.cognome
-FROM Persona as p, AttivitaProgetto as ap, Assenza as a
-WHERE ap.persona = p.id
-AND a.persona = p.id
-AND ap.giorno = a.giorno;
-
-SELECT DISTINCT p.nome, p.cognome, ap.giorno, pr.nome AS nome_progetto, a.tipo AS causa_assenza, ap.oreDurata AS durata_attivita_progettuale
-FROM Persona as p, AttivitaProgetto as ap, Assenza as a, Progetto as pr
-WHERE ap.persona = p.id
-AND a.persona = p.id
-AND ap.giorno = a.giorno
-AND ap.progetto = pr.id;
-
-SELECT wp1.nome
-FROM WP as wp1, WP as wp2
-WHERE wp1.nome = wp2.nome
-AND wp1.progetto <> wp2.progetto;
+SELECT DISTINCT Volo.comp
+FROM Volo, ArrPart, Compagnia
+WHERE Volo.codice = ArrPart.codice
+  AND ArrPart.partenza = 'FCO'
+  AND ArrPart.arrivo = 'JFK'
+  AND Compagnia.nome = Volo.comp
+  AND Compagnia.annoFondaz IS NOT NULL;
